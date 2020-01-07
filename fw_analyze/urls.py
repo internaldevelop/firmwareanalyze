9fb8d0408e3dbbc98353ed5c4b6fef1f1b1928ea
lvjz2018 <lvjianzhang@sina.com>
2020-01-07 17:20:18 +0800

    firmware convertcode

--- fw_analyze/urls.py
+++ fw_analyze/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('binwalk_scan_signature/', views.binwalk_scan_signature, name='firmware_binwalk'), # 固件文件头自动解码或解析
-    path('binwalk_scan_opcodes/', views.binwalk_scan_opcodes, name='firmware_binwalk'),     # 架构识别
-    path('binwalk_file_extract/', views.binwalk_file_extract, name='firmware_binwalk'),     # 抽取文件
-    path('binwalk_file_test/', views.binwalk_file_test, name='firmware_binwalk'),  # 抽取文件
+    path('decode/', views.binwalk_scan_signature, name='firmware_binwalk'),     # 固件文件头自动解码或解析
+    path('arch/', views.binwalk_scan_opcodes, name='firmware_binwalk'),         # 架构识别
+    path('extract/', views.binwalk_file_extract, name='firmware_binwalk'),      # 抽取文件
+    path('test/', views.binwalk_file_test, name='firmware_binwalk'),            # 抽取文件
+
+    path('convertcode/', views.angr_convert_code, name='angr_convert_code'),    # 转换成中间代码
+    path('recognize/', views.angr_recognize, name='angr_recognize'),            # 函数识别
+
 ]
--- fw_analyze/views.py
+++ fw_analyze/views.py
@@ -6,10 +6,12 @@ from common.utils.http_request import req_get_param
 from common.response import app_ok_p, app_err, sys_app_ok_p, sys_app_err
 from common.error_code import Error
 import binwalk
+import angr
 
 
 def index(request):
     return HttpResponse("Hello firmware analyze.")
+
 
 # 固件文件头自动解码或解析
 def binwalk_scan_signature(request):
@@ -23,7 +25,8 @@ def binwalk_scan_signature(request):
                 print("\t%s    0x%.8X    %s" % (result.file.path, result.offset, result.description))
     except binwalk.ModuleException as e:
         print("Critical failure:", e)
-    return sys_app_ok_p({'binwalk OK.': result_list})
+    return sys_app_ok_p({'decode': result_list})
+
 
 # 架构识别
 def binwalk_scan_opcodes(request):
@@ -52,6 +55,7 @@ def binwalk_scan_opcodes(request):
         print("Critical failure:", e)
         return sys_app_err('ERROR_INTERNAL_ERROR')
     return sys_app_ok_p({'structure': structure,})
+
 
 # 抽取文件
 def binwalk_file_extract(request):
@@ -103,3 +107,81 @@ def binwalk_file_test(request):
         print("Critical failure:", e)
         return sys_app_err('ERROR_INTERNAL_ERROR')
     return sys_app_ok_p({'extract': 'ok',})
+
+
+# binwalk 识别架构
+def getarch(filename):
+    structure = ''
+    try:
+        for module in binwalk.scan(filename, opcodes=True, quiet=True):
+            print("%s Results:" % module.name)
+            for result in module.results:
+                print("\t%s    0x%.8X    %s" % (result.file.path, result.offset, result.description))
+                if ("X86" in result.description.upper()):
+                    structure = 'x86'
+                    break
+                elif ("ARM" in result.description.upper()):
+                    structure = "arm"
+                    break
+                elif ("MIPS" in result.description.upper()):
+                    structure = "mips"
+                    break
+                else:
+                    structure = "powerpc"
+                    break
+
+    except binwalk.ModuleException as e:
+        print("Critical failure:", e)
+        return ""
+    return structure
+
+
+# 转换成中间代码
+def angr_convert_code(request):
+    try:
+        filename = req_get_param(request, 'filename')
+        arch = getarch(filename)
+        proj = angr.Project(filename, load_options={
+            'main_opts': {
+                'backend': 'blob',
+                'base_addr': 0,
+                'arch': arch,
+            },
+        })
+
+        print(proj.arch)
+        state = proj.factory.entry_state()
+
+        #### Blocks
+        block = proj.factory.block(proj.entry)       # lift a block of code from the program's entry point
+        pp = block.pp()                        # pretty-print a disassembly to stdout
+        print(block.instructions)              # how many instructions are there?
+        print(block.instruction_addrs)         # what are the addresses of the instructions?
+        print(block.capstone)                  # capstone disassembly
+        print(block.vex)                       # VEX IRSB (that's a python internal address, not a program address)
+
+    except binwalk.ModuleException as e:
+        print("Critical failure:", e)
+        return sys_app_err('ERROR_INTERNAL_ERROR')
+    return sys_app_ok_p({'code': str(block.vex),})
+
+
+# 函数识别
+def angr_recognize(request):
+
+    try:
+        filename = req_get_param(request, 'filename')
+        arch = getarch(filename)
+        proj = angr.Project(filename, load_options={
+            'main_opts': {
+                'backend': 'blob',
+                'base_addr': 0,
+                'arch': arch,
+            },
+        })
+
+
+    except binwalk.ModuleException as e:
+        print("Critical failure:", e)
+        return sys_app_err('ERROR_INTERNAL_ERROR')
+    return sys_app_ok_p({'functions': "",})
